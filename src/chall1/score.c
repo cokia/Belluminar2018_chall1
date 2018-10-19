7a851f8a8834058157904885cd3850036956fab0
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-10-20 00:55:43 +0900

    :bar_chart: UPDATE : print_scoreboard (sorting ranking as score)

--- src/chall1/score.c
+++ src/chall1/score.c
@@ -4,6 +4,17 @@
 #include <stdbool.h>
 #include "users.h"
 
+struct rank_struct {
+   char *id;
+   int score;
+};
+
+int compare (const void *s1, const void *s2){
+    struct rank_struct *r1 = (struct rank_struct *)s1;
+    struct rank_struct *r2 = (struct rank_struct *)s2;
+    return r2->score - r1->score;
+}
+
 void print_scoreboard(){
     printf("===== SCOREBOARD =====\n");
     // prints the current scoreboard
@@ -13,8 +24,19 @@ void print_scoreboard(){
         printf("score_db.txt: File not found\n");
         exit(0);
     }
+
+    // get file lines(len of rows)
+    int score_db_len = 0;
+    while (!feof(score_db)){
+        char ch = fgetc(score_db);
+        if(ch == '\n') score_db_len++;
+    }
+    rewind(score_db);
+    
+    struct rank_struct scoreboard[score_db_len];
+    int scoreboard_idx = 0;
+
     char *temp[100];
-    int rank = 1;
     while(fgets(temp, sizeof temp, score_db) != NULL){
         // score_db.txt => id:score
         char *ch;
@@ -22,15 +44,21 @@ void print_scoreboard(){
         int i = 0;
         while (ch != NULL){
             if (i == 0) // id
-                printf("[%d] %s - ", rank, ch);
-            else // score
-                printf("%d\n", atoi(ch));
+                scoreboard[scoreboard_idx].id = strdup(ch);
+            else if (i == 1) // score
+                scoreboard[scoreboard_idx].score = atoi(ch);
             ch = strtok(NULL, ":");
             i++;
         }
-        rank++;
+        scoreboard_idx++;
     }
-    // does not sort scoreboard data for now -> have to fix later
+    
+    // sort scoreboard
+    qsort(scoreboard, scoreboard_idx, sizeof(struct rank_struct), compare);
+    for (int i = 0; i < scoreboard_idx; i++){
+        printf("[%d] %s - %d\n", i + 1, scoreboard[i].id, scoreboard[i].score);
+    }
+    // printing user infomation has to wait a little more :(
     fclose(score_db);
 }
 
