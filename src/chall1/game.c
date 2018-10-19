41898a31fb01671aa52c0dd35e2b277394c3c165
danielsmith0612 <danielsmith0612@gmail.com>
2018-10-19 22:04:14 +0900

    📖  DOC: now, users controllable as keyboard arrow key

--- src/chall1/game.c
+++ src/chall1/game.c
@@ -1,6 +1,22 @@
 #include "game.h"
 
-static void drawSpaceShip(unsigned int x)
+int getch()
+{
+	int c;
+	struct termios oldattr, newattr;
+
+	tcgetattr(STDIN_FILENO, &oldattr);           
+	newattr = oldattr;
+	newattr.c_lflag &= ~(ICANON | ECHO);         
+	newattr.c_cc[VMIN] = 1;                      
+	newattr.c_cc[VTIME] = 0;                     
+	tcsetattr(STDIN_FILENO, TCSANOW, &newattr);  
+	c = getchar();                               
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);  
+	return c;
+}
+
+static inline void drawSpaceShip(unsigned int x)
 {
         unsigned int width, height, blank;
         for(height = 0; height < STATIC_SPACE_SHIP_HEIGHT; height++){
@@ -11,17 +27,32 @@ static void drawSpaceShip(unsigned int x)
                 }
                 for(width = 0; width < STATIC_SPACE_SHIP_WIDTH; width++){
                         if(UserSpaceShip[height][width] == 1){
-                            printf("▲");
+                                printf("▲");
                         }
                         else if(UserSpaceShip[height][width] == 0){
-                            printf(" ");
+                                printf(" ");
                         }
                 }
         puts(" ");
         }
 }
-
 int main()
 {
-        drawSpaceShip(2);
+        unsigned int x = STATIC_MAP_WIDTH / 2;
+        while(1){
+                unsigned int inputKeyboard;
+                inputKeyboard = getch();
+                switch(inputKeyboard){
+                        case 67: {
+                                system("clear");
+                                drawSpaceShip(x = x + 1);
+                                break;
+                        }
+                        case 68: {
+                                system("clear");
+                                drawSpaceShip(x = x - 1);
+                                break;
+                        }
+                }
+        }
 }
