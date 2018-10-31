9c422c4315eaa8df379ab1f7acce4d97067c716a
9host1st <9host1st@gmail.com>
2018-10-31 20:51:53 +0900

    a

--- src/chall1/main.c
+++ src/chall1/main.c
@@ -2,9 +2,20 @@
 
 int main(){
 	my_page();
-	int score = invaderGame();
-	printf("score = %d\n", score);
-	save_score(score);
-	print_scoreboard();
+	int select_number = 0;
+	for(int i = 0; i <= 53; i++) {
+		int score = invaderGame();
+		printf("score = %d\n", score);
+		save_score(score);
+		print_scoreboard();
+		printf("Retry? (0: start, 1: exit): ");
+		scanf("%d", &select_number);
+		if(select_number == 0) {
+			continue;
+		}
+		else {
+			break;
+		}
+	}
 	return 0;
 }
---
+++ src/chall1/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <curses.h>
+int main() {
+	int num = getchar();
+	printf("%d", num);
+}
